01ba51e080bcaaaf738f448a330cb9664ff89b35
Max Gerber <mgerber94@gmail.com>
2018-03-15 16:37:17 +0000

    creating array of coords #60

--- public/ports.js
+++ public/ports.js
@@ -1,17 +1,33 @@
 window.onload = function() {
   var avatar = document.getElementById('avatar');
   var svgDoc = avatar.contentDocument;
-  var head = svgDoc.getElementById('feet-hotspot');
-  var bounding = head.getBoundingClientRect();
-  var coords = {
-    bottom: bounding.bottom,
-    height: bounding.height,
-    left: bounding.left + window.scrollX,
-    right: bounding.right,
-    top: bounding.top + window.scrollY,
-    width: bounding.width,
-    x: bounding.x,
-    y: bounding.y
-  };
+  var hotspotBodyParts = [
+    'feet',
+    'legs',
+    'hands',
+    'belly',
+    'arms',
+    'chest',
+    'shoulders',
+    'face',
+    'head'
+  ];
+  var hotspotCoords = hotspotBodyParts.map(function(bodypart) {
+    var hotspot = svgDoc.getElementById(bodypart + '-hotspot');
+    var bounding = hotspot.getBoundingClientRect();
+    var coords = {
+      name: bodypart,
+      bottom: bounding.bottom,
+      height: bounding.height,
+      left: bounding.left + window.scrollX,
+      right: bounding.right,
+      top: bounding.top + window.scrollY,
+      width: bounding.width,
+      x: bounding.x,
+      y: bounding.y
+    };
+    return coords;
+  });
+  console.log('coords', hotspotCoords);
   app.ports.recieveHotspotCoords.send(coords);
 };
