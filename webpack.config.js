01a1e6cb047cd5b608cc17cc41c6ba8cf23fc5d0
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-03-15 12:26:19 +0000

    Add webpack to bundle frontend js files

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'eval',
+  mode: 'development'
+};
