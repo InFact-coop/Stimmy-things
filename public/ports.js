edd491c47b58cc1a756f0a641efb92a0e4c59fbd
sophielevens <sophielevens1@gmail.com>
2018-03-15 11:04:37 +0000

    solved cross broswer compatibility relates #53

--- public/ports.js
+++ public/ports.js
@@ -1,11 +1,13 @@
 app.ports.begin.subscribe(function() {
-  window.requestAnimationFrame(function() {
-    var element = document.getElementsByClassName('makecarousel')[0];
-    console.log('ELEMENT', element);
-    var flkty = new Flickity(element, {
-      wrapAround: true,
-      prevNextButtons: false,
-      pageDots: true
-    });
-  });
+	window.requestAnimationFrame(function() {
+		var element = document.getElementsByClassName("makecarousel")[0];
+		console.log("ELEMENT", element);
+		var flkty = new Flickity(element, {
+			lazyLoad: true,
+			adaptiveHeight: true,
+			wrapAround: true,
+			prevNextButtons: false,
+			pageDots: true
+		});
+	});
 });
--- src/elm/Views/CreateAvatar.elm
+++ src/elm/Views/CreateAvatar.elm
@@ -1,8 +1,8 @@
 module Views.CreateAvatar exposing (..)
 
+import Helpers exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Helpers exposing (..)
 import Types exposing (..)
 
 
@@ -12,9 +12,27 @@ createAvatar model =
         [ div [ class "flex justify-center flex-column items-center" ] [ h1 [ class "f3 green avenir center ma0" ] [ text "My Stimmy friend" ], h2 [ class "ma0 avenir fw2 f4" ] [ text "Choose your avatar" ] ]
         , div
             [ class "db makecarousel mv2" ]
-            [ div [ class "carousel-cell br4", backgroundImageStyle "./assets/avatar.svg" 100 ] []
-            , div [ class "carousel-cell br4", backgroundImageStyle "./assets/avatar.svg" 100 ] []
-            , div [ class "carousel-cell br4", backgroundImageStyle "./assets/avatar.svg" 100 ] []
-            , div [ class "carousel-cell br4", backgroundImageStyle "./assets/avatar.svg" 100 ] []
+            [ div [ class "carousel-cell br4" ]
+                [ img
+                    [ class "carousel-cell-image"
+                    , attribute "data-flickity-lazyload" "./assets/avatar.svg"
+                    ]
+                    []
+                ]
+            , img
+                [ class "carousel-cell-image"
+                , attribute "data-flickity-lazyload" "./assets/avatar.svg"
+                ]
+                []
+            , img
+                [ class "carousel-cell-image"
+                , attribute "data-flickity-lazyload" "./assets/avatar.svg"
+                ]
+                []
             ]
+        , img
+            [ class "carousel-cell-image"
+            , attribute "data-flickity-lazyload" "./assets/avatar.svg"
+            ]
+            []
         ]
