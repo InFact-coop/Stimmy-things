83556aa072a2095278c2c131d1798379fb0c3e2b
sophielevens <sophielevens1@gmail.com>
2018-03-19 21:54:35 +0000

    added borders and removed duplicate input field relates #65

--- src/elm/Views/AddStim.elm
+++ src/elm/Views/AddStim.elm
@@ -9,11 +9,11 @@ import Types exposing (..)
 
 addStim : Model -> Html Msg
 addStim model =
-    div [ class "main bg-washed-yellow" ]
+    div [ class "bg-washed-yellow" ]
         [ div
             [ class "bg-green flex flex-column items-center" ]
             [ button [] [ text "go back" ]
-            , p [ class "white f6 " ]
+            , p [ class "white f6 mb4" ]
                 [ text "Add new" ]
             , p [ class "white f6 mb3" ] [ text "chest" ]
             ]
@@ -22,11 +22,11 @@ addStim model =
             ]
         , div
             [ class "ma0" ]
-            [ Html.form [ class "flex flex-column f6" ]
+            [ Html.form [ class "flex flex-column f6 ml3 mr3" ]
                 [ label [ for "choose-exercise" ] []
-                , p [ class "f6 b" ]
+                , p [ class "f6 b " ]
                     [ text "Choose a part of the body" ]
-                , p [ class "" ] [ text "I want to calm my ..." ]
+                , p [] [ text "I want to calm my ..." ]
                 , p [ class "mb3" ] [ text " I am feeling anxious around my ..." ]
                 , textarea [ class "mb5", id "choose-exercise" ] [ text "Exercises go here" ]
                 , label [ for "exercise-name", class "b h6" ] [ text "what would you call this exercise?" ]
@@ -37,17 +37,16 @@ addStim model =
                 , div [ class "flex items-center justify-center" ]
                     [ img [ src "./assets/add_video_icn.svg", class " flex justify-center mb4" ] []
                     ]
-                , div [ class "flex flex-column items-center ba bw1 b--gray br3 pa3 mb3 ml3 mr3" ]
+                , div [ class "flex flex-column items-center ba bw1 b--gray br3 pa3 mb3 " ]
                     [ label [ class "b", for "add-a-video" ]
                         [ text "Add a video"
                         ]
-                    , p [ class "" ] [ text "Did you come across a video that might help you or your friends to follow this exercise? Search and add it here so that you can always go back to it later!" ]
+                    , p [] [ text "Did you come across a video that might help you or your friends to follow this exercise? Search and add it here so that you can always go back to it later!" ]
                     ]
                 , label [ for "youtube-search", class "mb2" ] [ text "Find a video on YouTube" ]
-                , textarea [ id "exercise-intstructions", placeholder "hand press" ] []
-                , div [ class "flex flex-column", class "h6" ]
-                    [ input [ type_ "text", onInput UpdateVideoSearch ] []
-                    , button [ onClick CallVideoRequest ] [ text "Search" ]
+                , div []
+                    [ input [ type_ "text", onInput UpdateVideoSearch, class "flex-column ba bw1 b--gray br1 mb3 pa1", placeholder "hand press" ] []
+                    , button [ onClick CallVideoRequest, class "flex bg-green br1 bn pa2" ] [ text "Search" ]
                     , div [] (List.map videoYT model.videos)
                     ]
                 , div [ class "flex flex-column justify-center" ]
