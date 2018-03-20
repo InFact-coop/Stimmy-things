b5b4507fa8429ada944720506954caa9caaaa2e9
sophielevens <sophielevens1@gmail.com>
2018-03-20 16:07:04 +0000

    further styling #65

--- src/elm/Views/AddStim.elm
+++ src/elm/Views/AddStim.elm
@@ -1,6 +1,7 @@
 module Views.AddStim exposing (..)
 
 import Components.Video exposing (videoYT)
+import Helpers.Style exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -17,39 +18,53 @@ addStim model =
                 [ text "Add new" ]
             , p [ class "white f6 mb3" ] [ text "chest" ]
             ]
-        , div [ class "flex items-center justify-center" ]
-            [ img [ src "./assets/zigzag_big_grey_down.svg", class " flex justify-center ma3" ] []
-            ]
         , div
             [ class "ma0" ]
-            [ Html.form [ class "flex flex-column f6 ml3 mr3" ]
-                [ label [ for "choose-exercise" ] []
-                , p [ class "f6 b " ]
-                    [ text "Choose a part of the body" ]
-                , p [] [ text "I want to calm my ..." ]
-                , p [ class "mb3" ] [ text " I am feeling anxious around my ..." ]
-                , textarea [ class "mb5", id "choose-exercise" ] [ text "Exercises go here" ]
-                , label [ for "exercise-name", class "b h6" ] [ text "what would you call this exercise?" ]
-                , textarea [ class "mb4", id "exercise-name" ] []
-                , label [ for "exercise-intstructions", class "b h6" ] [ text "how do you do this exercise?" ]
-                , p [] [ text "Be as specific as you can so that next time you want to do this exercise, you know exactly what to do." ]
-                , textarea [ class "mb5", id "exercise-intstructions" ] []
-                , div [ class "flex items-center justify-center" ]
+            [ Html.form [ class "flex flex-column f6" ]
+                [ div
+                    [ backgroundImageStyle "./assets/zigzag_choose_part_tags_bg.svg" 100
+                    , class "flex flex-column mb3 "
+                    ]
+                    [ img [ class "ma3", src "./assets/zigzag_big_grey_down.svg" ]
+                        []
+                    , label
+                        [ for "choose-exercise" ]
+                        []
+                    , p [ class "f6 b ml3 mr3" ]
+                        [ text "Choose a part of the body" ]
+                    , p [ class "ml3 mr3" ] [ text "I want to calm my ..." ]
+                    , p [ class "mb3 ml3 mr3" ] [ text " I am feeling anxious around my ..." ]
+                    , textarea [ class "mb5 bg-transparent bn h6", id "choose-exercise" ] [ text "" ]
+                    , p [ class "mb3 ml3 mr3" ] [ text "exercises go here" ]
+                    ]
+                , label [ for "exercise-name", class "b h6 ml3 mr3" ] [ text "what would you call this exercise?" ]
+                , textarea [ class "mb6 bg-transparent bn", id "exercise-name" ]
+                    []
+                , div [ backgroundImageStyle "./assets/zigzag_stim_how_to_bg.svg" 100, class "flex flex-column mb3" ]
+                    [ label [ for "exercise-intstructions", class "b h6 ml3 mr3" ]
+                        [ text "how do you do this exercise?" ]
+                    , p [ class "ml3 mr3" ]
+                        [ text "Be as specific as you can so that next time you want to do this exercise, you know exactly what to do." ]
+                    , textarea
+                        [ class "mb5 bg-transparent bn ml3 mr3", id "exercise-intstructions" ]
+                        []
+                    ]
+                , div [ class "flex flex-column ml3 mr3" ]
                     [ img [ src "./assets/add_video_icn.svg", class " flex justify-center mb4" ] []
-                    ]
-                , div [ class "flex flex-column items-center ba bw1 b--gray br3 pa3 mb3 " ]
-                    [ label [ class "b", for "add-a-video" ]
-                        [ text "Add a video"
+                    , div [ class "flex flex-column items-center justify-center  ba bw1 b--gray br3 pa3 mb3 ml3 mr3" ]
+                        [ label [ class "b", for "add-a-video" ]
+                            [ text "Add a video"
+                            ]
+                        , p [] [ text "Did you come across a video that might help you or your friends to follow this exercise? Search and add it here so that you can always go back to it later!" ]
                         ]
-                    , p [] [ text "Did you come across a video that might help you or your friends to follow this exercise? Search and add it here so that you can always go back to it later!" ]
+                    , label [ for "youtube-search", class "mb2 ml3 mr3" ] [ text "Find a video on YouTube" ]
+                    , div []
+                        [ input [ type_ "text", onInput UpdateVideoSearch, class "flex-column ba bw1 b--gray br1 ml3 mr3 mb3 pa1", placeholder "hand press" ] []
+                        , button [ onClick CallVideoRequest, class "flex bg-green br1 bn pa2 ml3 mr3" ] [ text "Search" ]
+                        , div [] (List.map videoYT model.videos)
+                        ]
                     ]
-                , label [ for "youtube-search", class "mb2" ] [ text "Find a video on YouTube" ]
-                , div []
-                    [ input [ type_ "text", onInput UpdateVideoSearch, class "flex-column ba bw1 b--gray br1 mb3 pa1", placeholder "hand press" ] []
-                    , button [ onClick CallVideoRequest, class "flex bg-green br1 bn pa2" ] [ text "Search" ]
-                    , div [] (List.map videoYT model.videos)
-                    ]
-                , div [ class "flex flex-column justify-center" ]
+                , div [ class "flex flex-column justify-center ml3 mr3" ]
                     [ img [ src "./assets/done_green_medium.svg" ] []
                     ]
                 ]
