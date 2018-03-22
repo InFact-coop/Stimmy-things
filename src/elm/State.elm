85fe635f11697c109ef5863b7631f119cd36d6a4
Lucy King <lucy.m.king92@gmail.com>
2018-03-22 23:11:19 +0000

    adds content, assets and links to about view relates to #84 #75

--- src/elm/State.elm
+++ src/elm/State.elm
@@ -17,7 +17,7 @@ import Update.Extra.Infix exposing ((:>))
 
 initModel : Model
 initModel =
-    { view = OnboardingFirst
+    { view = About
     , userId = ""
     , avatar = Avatar2
     , avatarName = "Sion"
