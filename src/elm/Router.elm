38530d44207f83c48e648d8201e4d53cfbc0e1a2
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-03-23 15:19:58 +0000

    Show right avatar on landing and fix styling height issues

--- src/elm/Router.elm
+++ src/elm/Router.elm
@@ -13,7 +13,7 @@ view model =
         view =
             getCurrentView model
     in
-        div [ class "w-100 mh-100 fixed overflow-y-scroll top-0 bottom-0 m0-auto cover bg-washed-yellow", id "container" ]
+        div [ class "w-100 fixed overflow-y-scroll top-0 bottom-0 m0-auto cover bg-washed-yellow", id "container" ]
             [ navDrawer model
             , view
             ]
