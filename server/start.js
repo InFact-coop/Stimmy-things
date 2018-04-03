76e3defef1a26c0a61d5776eadd6386eadcec446
Lucy King <lucy.m.king92@gmail.com>
2018-04-03 17:58:05 +0100

    Update start.js

--- server/start.js
+++ server/start.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`);
